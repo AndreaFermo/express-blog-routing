cf94ce4fa61731c324c87a1e7ae521b4d6e4735c
Andrea Fermo <andreafermo.sd@gmail.com>
2023-11-13 15:26:10 +0100

    risolto bug create

--- routers/posts.js
+++ routers/posts.js
@@ -4,9 +4,11 @@ const postsController = require("../controllers/posts");
 
 router.get('/', postsController.index);
 
+router.get('/create', postsController.create);
+
 router.get('/:slug', postsController.show);
 
-router.get('/create', postsController.create);
+
 
 router.get('/:slug/download', postsController.downloadImage);
 
